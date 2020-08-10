d761f7b23c3c6c4f61d8edd77171f58fc1a91516
Ke Liu <ke.liu1@thoughtworks.com>
2020-08-10 15:26:43 +0800

    [ke] add function collectCarNumberCount(collection) in startsWith file

--- 1-string/startsWith/index.js
+++ 1-string/startsWith/index.js
@@ -1,3 +1,12 @@
 export default function collectCarNumberCount(collection) {
   // TODO 7: 在这里写实现代码
+  let index = 0;
+  let str1 = '';
+  for (let i = 0; i < collection.length; i++) {
+    str1 = collection[i].substring(0, 2);
+    if (str1 === '粤A') {
+      index++;
+    }
+  }
+  return index;
 }
