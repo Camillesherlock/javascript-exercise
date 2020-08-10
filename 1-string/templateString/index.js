10e2225f89ef5d4ff5def08d63a6ff3c6fe4182f
Ke Liu <ke.liu1@thoughtworks.com>
2020-08-10 20:22:11 +0800

    [ke] add function getscoreoutput()

--- 1-string/templateString/index.js
+++ 1-string/templateString/index.js
@@ -7,4 +7,8 @@ function getScore(id) {
 
 export default function getScoreOutput(person) {
   // TODO 8: 在这里写实现代码
+  const str1 = '你好，';
+  const score = getScore(person.id);
+  const score1 = String(score);
+  return `${str1 + person.lastName + person.firstName}！` + `你的考试成绩为${score1}分`;
 }
