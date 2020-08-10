a0423fb80ccb67cbdc633e0d8800d0882beb1c0a
Ke Liu <ke.liu1@thoughtworks.com>
2020-08-10 23:58:08 +0800

    [ke] add_worker_class

--- 4-class/practice_4/worker.js
+++ 4-class/practice_4/worker.js
@@ -1,1 +1,12 @@
 // TODO 17: 在这里写实现代码
+import Person from './person';
+
+export default class Worker extends Person {
+  constructor(name, age) {
+    super(name, age);
+  }
+
+  introduce() {
+    return `I am a Student. ` + `I am at Class ${this.klass}.`;
+  }
+}
