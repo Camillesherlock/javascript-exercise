35113ead1bd1940330d90a83aadb410784bb11ac
Ke Liu <ke.liu1@thoughtworks.com>
2020-08-10 22:10:25 +0800

    [ke] add_function_numberMapToWord(collection)

--- 2-array/map/index.js
+++ 2-array/map/index.js
@@ -1,3 +1,8 @@
 export default function numberMapToWord(collection) {
   // TODO 3: 在这里写实现代码
+  const result = [];
+  for (let i = 0; i < collection.length; i++) {
+    result.push(String.fromCharCode(96 + collection[i]));
+  }
+  return result;
 }
