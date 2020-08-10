bd5fedb79d7b88dd786c053cb3efe9f9cd4ea73e
Ke Liu <ke.liu1@thoughtworks.com>
2020-08-10 22:37:35 +0800

    [ke] add_function_countTypesNumber(source)

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,8 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  let result = 0;
+  for (const key in source) {
+    result += Number(source[key]);
+  }
+  return result;
 }
