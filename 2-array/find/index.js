bbd810e8a70b6f967009cbc297e1967d3e297e7d
Ke Liu <ke.liu1@thoughtworks.com>
2020-08-10 22:05:32 +0800

    [ke] add find00OldPerson(collection)

--- 2-array/find/index.js
+++ 2-array/find/index.js
@@ -1,3 +1,8 @@
 export default function find00OldPerson(collection) {
   // TODO 4: 在这里写实现代码
+  let result;
+  for (let i = 0; i < collection.length; i++) {
+    if (collection[i].age == 18) result = collection[i];
+  }
+  return result.name;
 }
