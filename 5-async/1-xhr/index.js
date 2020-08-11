fe9027b8ddc91b326eeef56e4c25ac8dbe905f09
Ke Liu <ke.liu1@thoughtworks.com>
2020-08-11 12:08:24 +0800

    [ke] modify xhr

--- 5-async/1-xhr/index.js
+++ 5-async/1-xhr/index.js
@@ -2,17 +2,20 @@ function fetchData(url, successCallback, errorCallback) {
   const xhr = new XMLHttpRequest();
   // <-- start
   // TODO 21: 通过XMLHttpRequest实现异步请求
-  xhr.open('GET', URL, true);
+  xhr.open('GET', url, true);
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4) {
       if (xhr.status === 200) {
-        // The request has been completed successfully
         successCallback(xhr.responseText);
       } else {
         errorCallback(xhr.statusText);
       }
     }
   };
+  /*xhr.onload = () =>{ successCallback(xhr.responseText);
+  };
+  xhr.error = () => {
+  errorCallback(xhr.statusText);};*/
   xhr.send();
   // end -->
 }
@@ -20,12 +23,10 @@ const URL = 'http://localhost:3000/api';
 fetchData(
   URL,
   result => {
-    console.log(1);
-    //console.log(JSON.parse(result).name);
-    //document.writeln(JSON.parse(result).name);
+    //console.log(1);
+    document.writeln(JSON.parse(result).name);
   },
   error => {
     console.error(error);
-    //console.error(error);
   }
 );
--- 5-async/2-promise/index.js
+++ 5-async/2-promise/index.js
@@ -1,4 +1,4 @@
-function fetchData(url) {
+/*function fetchData(url) {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     // <-- start
@@ -19,6 +19,22 @@ const URL = 'http://localhost:3000/api';
 fetchData(URL)
   .then(result => {
     document.writeln(JSON.parse(result).name);
+  })
+  .catch(error => {
+    console.error(error);
+  });*/
+/*fetch(URL).then(res => {console.log('success')}).catch(error => {console.log('failure')});*/
+function fetchData(url) {
+  // <-- start
+  // TODO 23: 通过Fetch API实现异步请求
+  return fetch(url).then(res => res.json());
+  // end -->
+}
+
+const URL = 'http://localhost:3000/api';
+fetchData(URL)
+  .then(result => {
+    document.writeln(result.name);
   })
   .catch(error => {
     console.error(error);
